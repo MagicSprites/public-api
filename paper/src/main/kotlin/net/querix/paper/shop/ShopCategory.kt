81e2157e72574e0d8f5ab241f55d28810993cab9
dynomake <me@dynomake.space>
2024-09-10 20:00:15 +0300

    fix shop system

--- paper/src/main/kotlin/net/querix/paper/shop/ShopCategory.kt
+++ paper/src/main/kotlin/net/querix/paper/shop/ShopCategory.kt
@@ -6,7 +6,6 @@ import org.bukkit.inventory.ItemStack
 interface ShopCategory {
     fun items(): Collection<ShopElement>
     fun addElement(element: ShopElement)
-    fun open(player: Player)
     fun id(): String
     fun item(): ItemStack
 }
